7b59091deea2bbbfcb82a4a3e36586f686bf009e
Szymon Oleszek <szymqwerty@gmail.com>
2023-12-09 01:19:25 +0100

    intro with simple example

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const products = [
+  {
+    id: 1,
+    title: "iPhone 9",
+    price: 549,
+    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+  },
+  {
+    id: 2,
+    title: "iPhone X",
+    price: 899,
+    thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+  },
+  {
+    id: 3,
+    title: "Samsung Universe 9",
+    price: 1249,
+    thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+  },
+  {
+    id: 4,
+    title: "OPPOF19",
+    price: 280,
+    thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+  },
+];
+
+/**
+ * 1. funckja render
+ * 2. funckja do tworzenia produktu
+ * 3. funckja do tworzenia listy produktów
+ * 4. wyświetlenie na podstawie arrayki
+ * 5. pobranie danych i wyświetlenie pobranych danych
+ * https://dummyjson.com/products
+ */
