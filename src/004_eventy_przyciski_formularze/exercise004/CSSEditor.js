5796fb3f6fd600114ecdce332df87bca6cf19746
Szymon Oleszek <szymqwerty@gmail.com>
2023-12-10 09:28:45 +0100

    update

--- src/004_eventy_przyciski_formularze/exercise004/CSSEditor.js
+++ src/004_eventy_przyciski_formularze/exercise004/CSSEditor.js
@@ -23,18 +23,14 @@ import React, { useState } from "react";
  */
 
 export const CSSEditor = () => {
-  const [backgroundColor, setBackgroundColor] = useState("green");
-  const [justifyContentValue, setJustifyContentValue] = useState("flex-start");
-
-  const handleRadioButtonChange = (e) => {
-    setJustifyContentValue(e.target.value);
-  };
+  const [backgroundColor] = useState("green");
+  const [] = useState("flex-start");
 
   return (
     <article>
       <p>CSSEditor</p>
       <select
-        onChange={(e) => setBackgroundColor(e.target.value)}
+        onChange={(e) => console.log(e.target.value)}
         value={backgroundColor}
         name=""
         id=""
@@ -52,33 +48,7 @@ export const CSSEditor = () => {
       <input type="range" min={0} max={100} step={10} />
 
       <label>
-        <input
-          onChange={handleRadioButtonChange}
-          type="radio"
-          name="flex"
-          value="flex-start"
-        />{" "}
-        Flex-start
-      </label>
-
-      <label>
-        <input
-          onChange={handleRadioButtonChange}
-          type="radio"
-          name="flex"
-          value="center"
-        />{" "}
-        Center
-      </label>
-
-      <label>
-        <input
-          onChange={handleRadioButtonChange}
-          type="radio"
-          name="flex"
-          value="flex-end"
-        />{" "}
-        Flex-end
+        <input type="radio" name="flex" value="flex-start" /> Flex-start
       </label>
 
       <b style={{ marginTop: 30, display: "block" }}>Preview</b>
@@ -88,7 +58,7 @@ export const CSSEditor = () => {
           padding: 20,
           margin: 20,
           border: "2px solid red",
-          justifyContent: justifyContentValue,
+          /* tu powinna być wartość dynamiczna sterująca położeniem */
         }}
       >
         <div
